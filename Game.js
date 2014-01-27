9311a7ae7f7501d07a7418aafab220e6fc0f2353
benoitkoenig <benoit.koenig@yahoo.fr>
2014-01-27 11:16:13 +0100

    Ajout du fichier serveur et gesin des sockets de connection et de deplacement

---
+++ Game.js
@@ -0,0 +1,23 @@
+exports.Game = function() {
+	this.guys = [];
+
+	this.appendGuy = function(socket) {
+		this.guys.push(new Guy(socket));
+		for (var i=0 ; i<this.guys.length ; i++) {
+			socket.emit("popGuy", {x: guy.x, y: guy.y});
+		}
+		socket.emit("id", this.guys.length-1);
+		socket.broadcast.emit("popGuy", {x: this.x, y: this.y});
+	}
+}
+
+var Guy = function(socket) {
+	this.socket = socket;
+	var that = this;
+	this.x = 0;
+	this.y = 0;
+
+	socket.on("iMove", function(data) {
+		socket.broadcast.emit("iMove", {id: that.id, data: data});
+	});
+}
