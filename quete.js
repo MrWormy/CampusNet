8f77d13f79b5b9d313af42fa388dbb106a694fe6
benoitkoenig <benoit.koenig@yahoo.fr>
2014-03-23 20:39:02 +0100

    ajout des pnj et quete

---
+++ quete.js
@@ -0,0 +1,12 @@
+exports.liste = [
+	{
+		"id": 0,
+		"status": "unlocked",
+		"objet": {
+			"type": "parler_pnj",
+			"id_pnj": 0,
+			"dialogue": "Salut  Tu viens pour cotiser ? Cool !"
+		},
+		"finish": []
+	}
+]
