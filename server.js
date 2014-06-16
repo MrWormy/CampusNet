52a0e39b24ac5ab78882d85dd5d362ac81012b67
benoitkoenig <benoit.koenig@yahoo.fr>
2014-06-16 11:32:57 +0200

    Ajout de la modification d'avatars dans server.js

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ var express = require( 'express' ),
   io = require( 'socket.io' )( server ),
   play = require("./Game.js"),
   game = new play.Game(),
-  fs = require("node-fs");
+  fs = require("node-fs"),
+  mysql = require("mysql");;
+
+if (true) {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root"
+  });
+  connection.connect();
+}
 
 server.listen( 19872 );
 
@@ -27,6 +37,18 @@ app.use('/modifQuetes', function(req, res) {
   fs.writeFile("quete.js", req.query.valeurjson);
   res.sendfile( __dirname + '/modifQuetes.html' );
 });
+
+app.use('/modifAvatar', function(req, res) {
+  // TODO et c'est ultra important : vérifier l'identité CAS. C'est pas compliqué mais c'est ultra-important
+
+  var requete = "UPDATE `campusnet`.`users` SET AVATAR='"+req.query.avatar+"' WHERE `login`="+req.session.login+";";
+  connection.query(requete, function(err, rows, fields) {
+    if (err) throw err;
+  });
+
+  res.sendfile( __dirname + '/modifQuetes.html' );
+});
+
 app.get( '/quete.js', function ( req, res ) {
   res.sendfile( __dirname + '/quete.js' );
 } );
